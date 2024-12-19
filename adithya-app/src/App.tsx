a0d3d658d3d09e0c4b5515c12e9ff4d65046e1bc
SADHU ADITHYA <adithyasadhu123@gmail.com>
2024-12-19 20:07:25 +0530

    first commit

---
+++ adithya-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import Movies from './components/Movies'
+
+export default function App() {
+  return (
+    <div>
+
+      <Movies />
+      
+
+    </div>
+  )
+}
